25f15c170e455143957cfabd9af10c02473cca75
oliveiraeverson406-tech <oliveiraeverson406@gmail.com>
2026-08-22 09:43:12 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sacrificio-v5";
+const CACHE = "sacrificio-v6";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
